c50a0ce0327031742d33c5572188605eb7d7f50b
Festus Ali <festusali@gmail.com>
2026-06-04 13:39:05 +0100

    Ensure Products and Categories Are Initiated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useCategoriesStore } from './stores/categories'
+import { useProductsStore } from './stores/products'
+
+const productsStore = useProductsStore()
+const categoriesStore = useCategoriesStore()
+
+onMounted(() => {
+  if (!productsStore.products.length) {
+    productsStore.fetchProducts?.()
+  }
+  if (!categoriesStore.categories.length) {
+    categoriesStore.fetchCategories?.()
+  }
+})
+</script>
+
 <template>
   <router-view />
 </template>
